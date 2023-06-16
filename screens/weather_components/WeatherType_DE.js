ffa6bea51fcd32e277eeee84c1e11e0a33750976
Jacob Heller <136270759+JacobHellerDev@users.noreply.github.com>
2023-06-16 06:15:27 -0700

    Update WeatherType_DE.js

--- screens/weather_components/WeatherType_DE.js
+++ screens/weather_components/WeatherType_DE.js
@@ -1,3 +1,6 @@
+// onclick component for weather type
+// arrays for weather descriptions in multiple languages should be replaced with an external file
+
 import React, { useEffect, useState } from 'react'
 import { Text, StyleSheet, TouchableOpacity, View, Alert, ActivityIndicator } from 'react-native'
 import * as Location from 'expo-location'
