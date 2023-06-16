7aa662a76421e052c36ff2dcebce0bcf1de4dfbd
Jacob Heller <136270759+JacobHellerDev@users.noreply.github.com>
2023-06-16 06:09:55 -0700

    Update WeatherData_DE.js

--- screens/weather_components/WeatherData_DE.js
+++ screens/weather_components/WeatherData_DE.js
@@ -1,3 +1,5 @@
+// onclick menu for weather data
+
 import React, { useEffect, useState } from 'react'
 import { Text, StyleSheet, TouchableOpacity, View, Alert, ActivityIndicator } from 'react-native'
 import * as Location from 'expo-location'
