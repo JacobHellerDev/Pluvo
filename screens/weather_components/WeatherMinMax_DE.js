9a2dd3fbbfab4e3ff2f1a44127f2332c97731613
Jacob Heller <136270759+JacobHellerDev@users.noreply.github.com>
2023-06-16 06:10:27 -0700

    Update WeatherMinMax_DE.js

--- screens/weather_components/WeatherMinMax_DE.js
+++ screens/weather_components/WeatherMinMax_DE.js
@@ -1,3 +1,5 @@
+// onclick component for high / low temp
+
 import React, { useEffect, useState } from 'react'
 import { Text, StyleSheet, TouchableOpacity, View, Alert, ActivityIndicator } from 'react-native'
 import * as Location from 'expo-location'
